e59c0fe832b68ab354f91b507d67587042fe3463
ddddwee1 <cy960823@outlook.com>
2018-03-23 10:02:02 +0800

    aa

--- app/src/main/java/com/potato/word/take_photo/MainActivity.java
+++ app/src/main/java/com/potato/word/take_photo/MainActivity.java
@@ -4,18 +4,25 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -23,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     ImageView mImage = null;
     Button mButton = null;
     String mPhotoPath = null;
+    Button mUpload = null;
+    String photoStr = "abc";
+    Bitmap mBitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +41,19 @@ public class MainActivity extends AppCompatActivity {
 
         mImage = (ImageView) findViewById(R.id.imageView1);
         mButton = (Button) findViewById(R.id.button1);
+        mUpload = (Button) findViewById(R.id.button2);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dispatchTakePicture();
+            }
+        });
+
+        mUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                connect();
             }
         });
     }
@@ -102,5 +120,62 @@ public class MainActivity extends AppCompatActivity {
         mImage.setImageBitmap(bitmap);
     }
 
+    public void connect(){
+        new AsyncTask<Void,Void,String>(){
+            @Override
+            protected void onPostExecute(String s) {
+                mUpload.setText(s);
+                photoStr = s;
+                getBitMap();
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                Socket skt = null;
+                BufferedReader br = null;
+                BufferedWriter bw = null;
+                String msg = "";
+                String host = "192.168.1.105";
+                System.out.println("Start connection");
+                try{
+                    skt = new Socket(host,8080);
+                    br = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+                    bw = new BufferedWriter(new OutputStreamWriter(skt.getOutputStream()));
+                    bw.write(photoStr);
+                    bw.flush();
+                    String result;
+                    //while ((result=br.readLine())!=null){
+                       // msg += result;
+                    //}
+                    msg = br.readLine();
+                    System.out.println(msg);
+
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+                return msg;
+            }
+        }.execute();
+    }
+
+    public void getBitMap(){
+        byte[] decodedString = Base64.decode(photoStr,Base64.DEFAULT);
+        mBitmap = BitmapFactory.decodeByteArray(decodedString,0,decodedString.length);
+
+        // Get the dimensions of the View
+        int targetW = mImage.getWidth();
+        int targetH = mImage.getHeight();
+
+        int photoW = mBitmap.getWidth();
+        int photoH = mBitmap.getHeight();
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        targetH = photoH * scaleFactor;
+        targetW = photoW * scaleFactor;
+
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(mBitmap,targetW,targetH,false);
+        mImage.setImageBitmap(scaledBitmap);
+
+    }
 
 }
